159264d0411da872910aff6f93880725ba2561f3
Hsienlung <1635479595@qq.com>
2020-12-30 10:14:30 +0800

    完成了首页功能

--- assets/js/baseApi.js
+++ assets/js/baseApi.js
@@ -1,6 +1,23 @@
 
 //在执行ajax请求之前会执行这个方法
 $.ajaxPrefilter(function (options) {
-    console.log(1111);
-      options.url = "http://ajax.frontend.itheima.net" + options.url;
+    options.url = "http://ajax.frontend.itheima.net" + options.url;
+    
+
+    // 统一为有权限的接口，设置hearders
+    if (options.url.indexOf('/my/')!==-1) {
+        options.headers = {
+            Authorization: localStorage.getItem('token')
+        };
+    }
+
+    options.complete = function (res) {
+        console.log(res);
+        if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
+            //清除登录信息
+            localStorage.removeItem('token');
+            //返回登录页面
+            location.href = 'login.html';
+        }
+    };
   });
--- assets/js/index.js
+++ assets/js/index.js
@@ -1,16 +1,59 @@
 $(function () {
     getUserInfo();
+
+    let { layer } = layui;
+
+    // 退出功能
+    $('#btnLogout').on('click', function () {
+        //弹出提示信息
+        layer.confirm('确定退出登录？', {icon: 3, title:'提示'}, function(index){
+            //清除登录信息
+            localStorage.removeItem('token');
+            // 返回首页
+            location.href = 'login.html';
+            // layer.close(index);
+          });
+    })
 });
 
 function getUserInfo() {
     $.ajax({
-        url: 'http://ajax.frontend.itheima.net/my/userinfo',
+        url: '/my/userinfo',
         method: 'GET',
-        headers: {
-            Authorization: localStorage.getItem('token')
-        },
+        // headers: {
+        //     Authorization: localStorage.getItem('token')
+        // },
         success:function (res) {
-            console.log(res);
-        }
+          if (res.status !==0) {
+              return layui.layer.msg('获取用户信息失败！');
+            }
+            renderAvatar(res.data);
+        },
+        //不管请求成功与否都会执行
+        // complete:function (res) {
+        //     console.log(res);
+        //     if (res.responseJSON.status === 1&&res.responseJSON.message === '身份认证失败！') {
+        //         //清除登录信息
+        //         localStorage.removeItem('token');
+        //         //返回登录页面
+        //         location.href='login.html';
+        //     }
+        // }
     });
 }
+
+function renderAvatar(user) {
+    const name = user.nickname || user.username;
+    //渲染用户名
+    $('#welcome').html(`欢迎&nbsp;&nbsp;${name}`);
+    // 渲染头像
+    if (user.user_pic) {
+        $('.layui-nav-img').attr('src', user.user_pic).show();
+        $('.text-avatar').hide();
+    } else { 
+        const first = name[0].toUpperCase();//获取昵称首字母
+        $('.text-avatar').html(first).show();
+        $('.layui-nav-img').hide();
+    }
+}
+
